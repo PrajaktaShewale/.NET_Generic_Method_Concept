d737d2860514e19a52e380aa59eeb3982e8c859f
PrajaktaShewale <shewalep94@gmail.com>
2022-11-15 14:37:09 +0530

    [Prajakta]Add:ProblemToPrintArrays

---
+++ GenericDemo/GenericOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class GenericOperation
+    {
+        public void ToPrintArray(int[] intArr)
+        {
+            foreach(var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(double[] doubleArr)
+        {
+            foreach (var data in doubleArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(char[] intArr)
+        {
+            foreach (var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+    }
+}
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace GenericDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5, 6, };
+            double[] doubleArr = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+            GenericOperation generic = new GenericOperation();
+            generic.ToPrintArray(intArr);
+            generic.ToPrintArray(doubleArr);
+            generic.ToPrintArray(charArr);
+        }
+    }
+}

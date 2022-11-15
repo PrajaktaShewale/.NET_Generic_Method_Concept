ba6417d2cad44d424afd3966fd1e1869616bcf36
PrajaktaShewale <shewalep94@gmail.com>
2022-11-15 14:55:42 +0530

    [Prajakta]Add:ProblemToPrintArraysUsingGenericOperation

---
+++ GenericDemo/GenericOperationUse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class GenericOperationUse
+    {
+        public void ToPrintArray<T>(T[] intArr)
+        {
+            foreach(var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("Arrays Printed by Generic Operation");
+        }
+    }
+}
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -8,10 +8,10 @@ namespace GenericDemo
             int[] intArr = { 1, 2, 3, 4, 5, 6, };
             double[] doubleArr = { 1.5, 2.5, 3.5, 4.5, 5.5 };
             char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
-            GenericOperation generic = new GenericOperation();
-            generic.ToPrintArray(intArr);
-            generic.ToPrintArray(doubleArr);
-            generic.ToPrintArray(charArr);
+            GenericOperationUse generic = new GenericOperationUse();
+            generic.ToPrintArray<int>(intArr);
+            generic.ToPrintArray<double>(doubleArr);
+            generic.ToPrintArray<char>(charArr);
         }
     }
 }
